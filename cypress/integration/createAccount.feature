796de87552ce85182ebd9a3ef60a0018ab67ffa8
Prashanthi <prashanthi.purnima@gmail.com>
2022-06-24 20:11:41 +0100

    check password errors

--- cypress/integration/createAccount.feature
+++ cypress/integration/createAccount.feature
@@ -2,6 +2,7 @@ Feature: Create account
 
     Scenario: Create account and validations
 
-        Given User visit the url it should close cookies popup
+        Given User visit the url it should accept cookies popup
         When User fills the invalid Email address and password
         Then User should see validation errors
+        Then all three password errors should be displayed
--- cypress/integration/createAccount/createAccount.js
+++ cypress/integration/createAccount/createAccount.js
@@ -1,7 +1,7 @@
 import { Given, When, And, Then } from "cypress-cucumber-preprocessor/steps";
 import { AccountPage } from "../../support/PageObjects/AccountPage";
 
-Given("User visit the url it should close cookies popup", () => {
+Given("User visit the url it should accept cookies popup", () => {
     cy.acceptBTCookies()
 });
 
@@ -17,4 +17,12 @@ Then("User should see validation errors", () => {
     cy.get('[data-testid="password-feedback"]')
         .should('be.visible')
         .should('have.text', 'Choose a password without sequences of numbers or letters, like 123 or abc'); 
+})
+
+Then("all three password errors should be displayed", () => {
+    cy.get('[data-testid="password-requirements"] ul li')
+        .should('have.length', 3)
+        .each($elm => {
+            cy.wrap($elm).should('have.attr', 'data-invalid', 'true')
+        })
 })
